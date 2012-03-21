f74cb47c7d1e3360cc41b54b884873be7954b956
ajdeveloped@gmail.com <ajdeveloped@gmail.com@53b91c4f-db84-3a66-376e-a7ef90917ce7>
2012-03-21 16:54:52 +0000

    Filipino on Demand: Corrected Filipino on Demand movies section parsing.

--- addons/plugin.video.filipinotv/mymoves/movies/Pinoy_Movies.py
+++ addons/plugin.video.filipinotv/mymoves/movies/Pinoy_Movies.py
@@ -30,11 +30,11 @@ def displayMoviesMenu(request_obj, response_obj):
     item.add_request_data('movieCategoryUrl', 'http://www.pinoymovie.co/')
     xbmcListItem = xbmcgui.ListItem(label='Recently Added', iconImage=movies_icon_filepath, thumbnailImage=movies_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
-    response_obj.addListItem(item)
+    #response_obj.addListItem(item)
     
-    contentDiv = BeautifulSoup.SoupStrainer('div', {'id':'sub-sidebar'})
+    contentDiv = BeautifulSoup.SoupStrainer('div', {'id':'sidebar'})
     soup = HttpClient().getBeautifulSoup(url='http://www.pinoymovie.co/', parseOnlyThese=contentDiv)
-    soup = soup.findChild('div', {'class':'left'})
+    soup = soup.findChild('div', {'class':'right'})
     
     for liItemTag in soup.findChildren('li', {'class':re.compile(r'\bcat-item\b')}):
         aTag = liItemTag.findChild('a')
@@ -63,11 +63,11 @@ def __retrieveAndCreateMovieItem__(movieTag):
     thumbTag = movieTag.findChild('div', {'class':'thumbnail'})
     imgUrl = ''
     if thumbTag is not None:    
-        aTag = thumbTag.find('a')
-        imgTag = aTag.find('img')
+        aTag = thumbTag.findChild('a')
+        imgTag = aTag.findChild('img')
         imgUrl = imgTag['src']
     
-    titleTag = movieTag.findChild('h2', {'class':'titleindex'})
+    titleTag = movieTag.findChild('h2')
     aTag = titleTag.findChild('a')
     movieUrl = str(aTag['href'])
     title = unicode(titleTag.getText()).encode('utf8').replace('&#8217;', '\'').replace('&#038;', '&').replace('&#8230;', '...')
@@ -108,20 +108,23 @@ def displayMovies(request_obj, response_obj):
         if url_part_A[len(url_part_A) - 1] != '/':
             url_part_A = url_part_A + '/'
         url = url_part_A + 'page/' + request_obj.get_data()['page'] + url_part_B
-    
     contentDiv = BeautifulSoup.SoupStrainer('div', {'id':'content'})
     soup = HttpClient().getBeautifulSoup(url=url, parseOnlyThese=contentDiv)
     movieTags = soup.findChildren('div', {'class':'post'})
     for movieTag in movieTags:
         item = __retrieveAndCreateMovieItem__(movieTag)
         response_obj.addListItem(item)
-        
+    
     response_obj.set_xbmc_content_type('movies')
     
-    pagesInfoTag = soup.findChild('span', {'class':'pages'}).getText()
-    pages = re.compile('Page (\d+) of (\d+)').findall(pagesInfoTag)
-    current_page = int(pages[0][0])
-    last_page = int(pages[0][1])
+    pagesInfoTag = soup.findChild('div', {'class':'navigation'})
+    print pagesInfoTag
+    current_page = int(pagesInfoTag.find('span', {'class':'page current'}).getText())
+    print current_page
+    pages = pagesInfoTag.findChildren('a', {'class':'page'})
+    print pages
+    last_page = int(pages[len(pages) - 1].getText())
+    
     if current_page < last_page:
         for page in range(current_page + 1, last_page + 1):
             createItem = False
@@ -173,28 +176,120 @@ def retrieveVideoLinks(request_obj, response_obj):
     url = request_obj.get_data()['movieUrl']
     contentDiv = BeautifulSoup.SoupStrainer('div', {'class':'video'})
     soup = HttpClient().getBeautifulSoup(url=url, parseOnlyThese=contentDiv)
-    videoFrameLinks = re.compile('http://www.pinoymovie.c(o|a)/ajaxtabs/(.+?).htm').findall(urllib.unquote(str(soup)))
-    video_source_id = 1
-    for ignoreIt, videoFrameLink in videoFrameLinks: #@UnusedVariable
-        try:
-            soup = HttpClient().getBeautifulSoup(url='http://www.pinoymovie.co/ajaxtabs/' + videoFrameLink + '.htm')
-            video_url = str(soup.find('iframe')['src'])
-            video_hosting_info = SnapVideo.findVideoHostingInfo(video_url)
-            if video_hosting_info is None:
-                print 'UNKNOWN streaming link found: ' + video_url
-            else:
-                video_source_img = video_hosting_info.get_video_hosting_image()
-                video_title = 'Source #' + str(video_source_id) + ' :: ' + video_hosting_info.get_video_hosting_name()
-                
-                item = ListItem()
-                item.add_request_data('videoLink', video_url)
-                item.add_request_data('videoTitle', video_title)
-                item.set_next_action_name('SnapAndPlayVideo')
-                xbmcListItem = xbmcgui.ListItem(label=video_title, iconImage=video_source_img, thumbnailImage=video_source_img)
-                item.set_xbmc_list_item_obj(xbmcListItem)
-                response_obj.addListItem(item)
-                video_source_id = video_source_id + 1
-        except:
-            print 'UNKNOWN streaming link found'
-
+    decodedSoup = urllib.unquote(str(soup))
+    videoFrameLinks = re.compile('http://www.pinoymovie.c(o|a)/ajaxtabs/(.+?).htm').findall(decodedSoup)
+    if len(videoFrameLinks) > 0:
+        video_source_id = 1
+        for ignoreIt, videoFrameLink in videoFrameLinks: #@UnusedVariable
+            try:
+                soup = HttpClient().getBeautifulSoup(url='http://www.pinoymovie.co/ajaxtabs/' + videoFrameLink + '.htm')
+                video_url = str(soup.find('iframe')['src'])
+                video_hosting_info = SnapVideo.findVideoHostingInfo(video_url)
+                if video_hosting_info is None:
+                    print 'UNKNOWN streaming link found: ' + video_url
+                else:
+                    video_source_img = video_hosting_info.get_video_hosting_image()
+                    video_title = 'Source #' + str(video_source_id) + ' :: ' + video_hosting_info.get_video_hosting_name()
+                    
+                    item = ListItem()
+                    item.add_request_data('videoLink', video_url)
+                    item.add_request_data('videoTitle', video_title)
+                    item.set_next_action_name('SnapAndPlayVideo')
+                    xbmcListItem = xbmcgui.ListItem(label=video_title, iconImage=video_source_img, thumbnailImage=video_source_img)
+                    item.set_xbmc_list_item_obj(xbmcListItem)
+                    response_obj.addListItem(item)
+                    video_source_id = video_source_id + 1
+            except:
+                print 'UNKNOWN streaming link found'
+    else:
+        videoLinks = re.compile('flashvars=(.+?)file=(.+?)&').findall(decodedSoup)
         
+        moreLinks = re.compile('<iframe(.+?)src="(.+?)"', flags=re.I).findall(decodedSoup)
+        if len(moreLinks) > 0:
+            videoLinks.extend(moreLinks)
+        
+        moreLinks = re.compile('<a(.+?)href="(.+?)"', flags=re.I).findall(decodedSoup)
+        if len(moreLinks) > 0:
+            videoLinks.extend(moreLinks)
+        
+        if len(videoLinks) > 0:
+            
+            video_source_id = 1
+            video_source_img = None
+            video_part_index = 0
+            video_playlist_items = []
+            for ignoreIt, videoLink in videoLinks: #@UnusedVariable
+                try:
+                    if re.search('http://media.pinoymovie.ca/playlist/(.+?).xml', videoLink, re.I):
+                        soupXml = HttpClient().getBeautifulSoup(url=videoLink)
+                        
+                        for media in soupXml.findChildren('track'):
+                            video_url = media.findChild('location').getText()
+                            video_hosting_info = SnapVideo.findVideoHostingInfo(video_url)
+                            
+                            if video_hosting_info is None:
+                                print 'UNKNOWN streaming link found: ' + video_url
+                            else:
+                                
+                                video_part_index = video_part_index + 1
+                                video_link = {}
+                                video_link['videoTitle'] = 'Source #' + str(video_source_id) + ' | ' + 'Part #' + str(video_part_index)
+                                video_link['videoLink'] = video_url
+                                video_link['videoSourceImg'] = video_hosting_info.get_video_hosting_image()
+                                
+                                video_playlist_items.append(video_link)
+                                video_source_img = video_link['videoSourceImg']
+                                
+                                
+                                item = ListItem()
+                                item.add_request_data('videoLink', video_link['videoLink'])
+                                item.add_request_data('videoTitle', video_link['videoTitle'])
+                                item.set_next_action_name('SnapAndPlayVideo')
+                                xbmcListItem = xbmcgui.ListItem(label=video_link['videoTitle'], iconImage=video_source_img, thumbnailImage=video_source_img)
+                                item.set_xbmc_list_item_obj(xbmcListItem)
+                                response_obj.addListItem(item)
+                        
+                        if len(video_playlist_items) > 0:
+                            response_obj.addListItem(__preparePlayListItem__(video_source_id, video_source_img, video_playlist_items))
+                            video_source_id = video_source_id + 1
+                            video_source_img = None
+                            video_part_index = 0
+                            video_playlist_items = []
+                    
+                    else:
+                        
+                        if re.search('http://media.pinoymovie.ca/playlist/(.+?).htm', videoLink, re.I):
+                            html = HttpClient().getHtmlContent(url=videoLink).replace('\'', '"')
+                            videoLink = re.compile('<iframe(.+?)src="(.+?)"', flags=re.I).findall(html)[0][0]
+                            
+                        video_hosting_info = SnapVideo.findVideoHostingInfo(videoLink)
+                        print videoLink
+                        if video_hosting_info is None:
+                            print 'UNKNOWN streaming link found: ' + videoLink
+                            
+                        else:
+                            item = ListItem()
+                            item.add_request_data('videoLink', videoLink)
+                            item.add_request_data('videoTitle', 'Source #' + str(video_source_id))
+                            item.set_next_action_name('SnapAndPlayVideo')
+                            xbmcListItem = xbmcgui.ListItem(label='Source #' + str(video_source_id), iconImage=video_hosting_info.get_video_hosting_image(), thumbnailImage=video_hosting_info.get_video_hosting_image())
+                            item.set_xbmc_list_item_obj(xbmcListItem)
+                            response_obj.addListItem(item)
+                            
+                            video_source_id = video_source_id + 1
+                except:
+                    print 'UNKNOWN streaming link found'
+                    video_source_img = None
+                    video_part_index = 0
+                    video_playlist_items = []
+                    
+
+
+def __preparePlayListItem__(video_source_id, video_source_img, video_playlist_items):
+    item = ListItem()
+    item.add_request_data('videoPlayListItems', video_playlist_items)
+    item.set_next_action_name('SnapAndDirectPlayList')
+    xbmcListItem = xbmcgui.ListItem(label=AddonUtils.getBoldString('DirectPlay') + ' | ' + 'Source #' + str(video_source_id) + ' | ' + 'Parts = ' + str(len(video_playlist_items)) , iconImage=video_source_img, thumbnailImage=video_source_img)
+    item.set_xbmc_list_item_obj(xbmcListItem)
+    return item
+
