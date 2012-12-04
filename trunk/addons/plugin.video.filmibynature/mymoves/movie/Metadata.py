45944c33b6eff615886035cd96be65691e039d5a
ajdeveloped@gmail.com <ajdeveloped@gmail.com@53b91c4f-db84-3a66-376e-a7ef90917ce7>
2012-12-04 18:16:54 +0000

    Filmi By Nature: Added more logs to understand the bug

--- trunk/addons/plugin.video.filmibynature/mymoves/movie/Metadata.py
+++ trunk/addons/plugin.video.filmibynature/mymoves/movie/Metadata.py
@@ -14,8 +14,8 @@ def __addMovieInfo_in_item(item):
     title = item.get_moving_data()['movieTitle']
     year = item.get_moving_data()['movieYear']
     print 'GOING to fetch metadata for movie :: TITLE =' + title + ' YEAR =' + year
-
     metaget = metahandlers.MetaData()
+    print 'Module used for metadata retrieval is metahandlers'
     meta = metaget.get_meta('movie', title, year=year)
     xbmc_item = item.get_xbmc_list_item_obj()
     
@@ -23,7 +23,7 @@ def __addMovieInfo_in_item(item):
         xbmc_item.setIconImage(meta['thumb_url'])
         xbmc_item.setThumbnailImage(meta['cover_url'])
         xbmc_item.setInfo('video', {'genre':meta['genre'], 'title':meta['title'], 'year':meta['year'], 'rating':meta['rating'], 'tagline':meta['tagline'], 'writer':meta['writer'] , 'director':meta['director'], 'cast':meta['cast'], 'raiting':meta['rating'], 'votes':meta['votes'], 'plot':meta['plot'], 'duration':meta['duration'], 'mpaa':meta['mpaa'], 'studio':meta['studio'] , 'premiered':meta['premiered'], 'trailer_url':meta['trailer_url']})
-        xbmc_item.setProperty('fanart_image',meta['backdrop_url'])
+        xbmc_item.setProperty('fanart_image', meta['backdrop_url'])
     else:
         xbmc_item.setInfo('video', {'title':title, 'year':year})
 
