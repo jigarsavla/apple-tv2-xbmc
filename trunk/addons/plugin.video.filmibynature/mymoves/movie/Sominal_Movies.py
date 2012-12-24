22179c2fb3fb386c89a3c5618b8c6da6dc478072
ajDeveloped@gmail.com <ajDeveloped@gmail.com@53b91c4f-db84-3a66-376e-a7ef90917ce7>
2012-12-24 19:58:29 +0000

    FIlmi By Nature: Enabled caching & pagination

--- trunk/addons/plugin.video.filmibynature/mymoves/movie/Sominal_Movies.py
+++ trunk/addons/plugin.video.filmibynature/mymoves/movie/Sominal_Movies.py
@@ -13,13 +13,20 @@ import sys
 import BeautifulSoup
 from moves import SnapVideo
 from common import XBMCInterfaceUtils
+import TurtlePlugin
 try:
     import json
 except ImportError:
     import simplejson as json
 from common import HttpUtils
+try:
+    import StorageServer
+except:
+    import storageserverdummy as StorageServer
 
 BASE_WSITE_URL = base64.b64decode('aHR0cDovL3d3dy5zb21pbmFsdHZmaWxtcy5jb20v')
+pageDict = {0:25, 1:50, 2:100}
+TITLES_PER_PAGE = pageDict[int(AddonContext().addon.getSetting('moviesPerPage'))]
 
 def displayMainMenu(request_obj, response_obj):
     # HD Movies
@@ -41,7 +48,7 @@ def displayMainMenu(request_obj, response_obj):
     # Hindi Movies
     hindi_movie_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='Hindi_Movies_V1.png')
     item = ListItem()
-    item.set_next_action_name('Hindi')
+    item.set_next_action_name('CategoryMovies')
     item.add_request_data('infoUrl', BASE_WSITE_URL + '2010/11/hindi-movies.html')
     xbmcListItem = xbmcgui.ListItem(label='HINDI', iconImage=hindi_movie_icon_filepath, thumbnailImage=hindi_movie_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
@@ -50,7 +57,7 @@ def displayMainMenu(request_obj, response_obj):
     # Telugu Movies
     telugu_movie_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='Telugu_Movies_V1.png')
     item = ListItem()
-    item.set_next_action_name('Telugu')
+    item.set_next_action_name('CategoryMovies')
     item.add_request_data('infoUrl', BASE_WSITE_URL + '2010/11/telugu-movies.html')
     xbmcListItem = xbmcgui.ListItem(label='TELUGU', iconImage=telugu_movie_icon_filepath, thumbnailImage=telugu_movie_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
@@ -59,7 +66,7 @@ def displayMainMenu(request_obj, response_obj):
     # Tamil Movies
     tamil_movie_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='Tamil_Movies_V1.png')
     item = ListItem()
-    item.set_next_action_name('Tamil')
+    item.set_next_action_name('CategoryMovies')
     item.add_request_data('infoUrl', BASE_WSITE_URL + '2010/11/tamil-movies.html')
     xbmcListItem = xbmcgui.ListItem(label='TAMIL', iconImage=tamil_movie_icon_filepath, thumbnailImage=tamil_movie_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
@@ -68,7 +75,7 @@ def displayMainMenu(request_obj, response_obj):
     # Punjabi Movies
     punjabi_movie_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='Movies_V1.png')
     item = ListItem()
-    item.set_next_action_name('Punjabi')
+    item.set_next_action_name('CategoryMovies')
     item.add_request_data('infoUrl', BASE_WSITE_URL + '2011/08/punjabi-movies.html')
     xbmcListItem = xbmcgui.ListItem(label='PUNJABI', iconImage=punjabi_movie_icon_filepath, thumbnailImage=punjabi_movie_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
@@ -77,20 +84,20 @@ def displayMainMenu(request_obj, response_obj):
     # Malayalam Movies
     malayalam_movie_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='Malayalam_Movies_V1.png')
     item = ListItem()
-    item.set_next_action_name('Malayalam')
+    item.set_next_action_name('CategoryMovies')
     item.add_request_data('infoUrl', BASE_WSITE_URL + '2010/11/hindi-movies.html')
     xbmcListItem = xbmcgui.ListItem(label='MALAYALAM', iconImage=malayalam_movie_icon_filepath, thumbnailImage=malayalam_movie_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
-    response_obj.addListItem(item)
+#    response_obj.addListItem(item)
     
     # Bengali Movies
     bengali_movie_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='Bengali_Movies_V1.png')
     item = ListItem()
-    item.set_next_action_name('Bengali')
+    item.set_next_action_name('CategoryMovies')
     item.add_request_data('infoUrl', BASE_WSITE_URL + '2010/11/hindi-movies.html')
     xbmcListItem = xbmcgui.ListItem(label='BENGALI', iconImage=bengali_movie_icon_filepath, thumbnailImage=bengali_movie_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
-    response_obj.addListItem(item)
+#    response_obj.addListItem(item)
 
 
 def displayUC(request_obj, response_obj):
@@ -98,25 +105,40 @@ def displayUC(request_obj, response_obj):
     XBMCInterfaceUtils.displayDialogMessage(heading='UNDER Construction', line1='Please wait for update!!', line2='Enjoy HD movies for the time being.', line3='')
 
 def listHDMovies(request_obj, response_obj):
-    html = HttpUtils.HttpClient().getHtmlContent(url=(BASE_WSITE_URL + "feeds/posts/summary/-/BluRay?max-results=10000&alt=json"))
-    jObj = json.loads(html)
-    print 'RETRIEVED MOVIE OBJECT'
-    print len(jObj["feed"]["entry"])
-    for entry in jObj["feed"]["entry"]:
-        titleInfo = str(entry["title"]["$t"])
+    cache = StorageServer.StorageServer(TurtlePlugin.__addon_id__, 12)
+    titles = cache.cacheFunction(retrieveHDMovies)
+    
+    count = -1
+    start = 0
+    current_page = -1
+    total_pages = -1
+    if len(titles) > TITLES_PER_PAGE:
+        count = 0
+        current_page = 1
+        total_pages = int(len(titles) / TITLES_PER_PAGE)
+        if len(titles) % TITLES_PER_PAGE:
+            total_pages = total_pages + 1
+        if request_obj.get_data().has_key('page'):
+            current_page = int(request_obj.get_data()['page'])
+            start = (current_page - 1) * TITLES_PER_PAGE
+    end = start + TITLES_PER_PAGE
+    for entry in titles:
+        if count > -1:
+            if count < start:
+                count = count + 1
+                continue
+            elif count >= end:
+                break
+            else:
+                count = count + 1
+        titleInfo = entry['info']
         print titleInfo
         movieInfo = re.compile("(.+?)\((\d+)\) (.*)").findall(titleInfo)
         title = unicode(movieInfo[0][0].rstrip()).encode('utf-8')
         year = unicode(movieInfo[0][1]).encode('utf-8')
         quality = unicode(movieInfo[0][2]).encode('utf-8')
-        moviePageUrl = ""
-        movieInfoUrl = ""
-        for link in entry["link"]:
-            if link["rel"] == "self":
-                movieInfoUrl = str(link["href"])
-            elif link["rel"] == "alternate":
-                moviePageUrl = str(link["href"])
-                
+        movieInfoUrl = entry["link"]
+        
         item = ListItem()
         item.add_moving_data('movieTitle', title)
         item.add_moving_data('movieYear', year)
@@ -125,23 +147,75 @@ def listHDMovies(request_obj, response_obj):
         xbmcListItem = xbmcgui.ListItem(label=title, label2='(' + year + ') - ' + quality)
         item.set_xbmc_list_item_obj(xbmcListItem)
         response_obj.addListItem(item)
+    
+    if current_page > 0 and current_page < total_pages:
+        next_page = current_page + 1
+        item = ListItem()
+        item.add_request_data('page', next_page)
+        item.set_next_action_name('Next_Page')
+        xbmcListItem = xbmcgui.ListItem(label='  ---- next page ----  #' + str(next_page) + ' ->')
+        item.set_xbmc_list_item_obj(xbmcListItem)
+        response_obj.addListItem(item)
+    
     response_obj.set_xbmc_content_type('movies')
+    
+'''
+Cached function to retrieve HD movies
+'''
+def retrieveHDMovies():
+    html = HttpUtils.HttpClient().getHtmlContent(url=(BASE_WSITE_URL + "feeds/posts/summary/-/BluRay?max-results=10000&alt=json"))
+    jObj = json.loads(html)
+    titles = []
+    print 'RETRIEVED MOVIE OBJECT'
+    print len(jObj["feed"]["entry"])
+    for entry in jObj["feed"]["entry"]:
+        titleInfo = str(entry["title"]["$t"])
+        print titleInfo
+        movieInfoUrl = ""
+        for link in entry["link"]:
+            if link["rel"] == "self":
+                movieInfoUrl = str(link["href"])
+                break
+        
+        title = {}
+        title['info'] = titleInfo
+        title['link'] = movieInfoUrl
+        titles.append(title)
+    return titles
 
 
 def listCategoryMovies(request_obj, response_obj):
-    contentDiv = BeautifulSoup.SoupStrainer('div', {'dir':'ltr'})
-    soup = HttpUtils.HttpClient().getBeautifulSoup(url=request_obj.get_data()['infoUrl'], parseOnlyThese=contentDiv)
-    for aTag in soup.findAll('a', {'href':re.compile('http://www.sominaltv(theater|films).com(.+?).html')}):
-        if not re.search('^(?!\()', aTag.getText()):
-            continue
-        titleInfo = aTag.getText()
-        print '     ' + aTag['href']
+    cache = StorageServer.StorageServer(TurtlePlugin.__addon_id__, 12)
+    titles = cache.cacheFunction(retrieveCategoryMovies, request_obj.get_data()['infoUrl'])
+    
+    count = -1
+    start = 0
+    current_page = -1
+    total_pages = -1
+    if len(titles) > TITLES_PER_PAGE:
+        count = 0
+        current_page = 1
+        total_pages = int(len(titles) / TITLES_PER_PAGE)
+        if len(titles) % TITLES_PER_PAGE:
+            total_pages = total_pages + 1
+        if request_obj.get_data().has_key('page'):
+            current_page = int(request_obj.get_data()['page'])
+            start = (current_page - 1) * TITLES_PER_PAGE
+    end = start + TITLES_PER_PAGE
+    for entry in titles:
+        if count > -1:
+            if count < start:
+                count = count + 1
+                continue
+            elif count >= end:
+                break
+            else:
+                count = count + 1
+        titleInfo = entry['info']
         movieInfo = re.compile("(.+?)\((\d+)\)").findall(titleInfo)
-        if not len(movieInfo) > 0:
-            continue
         title = unicode(movieInfo[0][0].rstrip()).encode('utf-8')
         year = unicode(movieInfo[0][1]).encode('utf-8')
-        moviePageUrl = aTag['href']
+        moviePageUrl = entry['link']
                 
         item = ListItem()
         item.add_moving_data('movieTitle', title)
@@ -151,9 +225,40 @@ def listCategoryMovies(request_obj, response_obj):
         xbmcListItem = xbmcgui.ListItem(label=title, label2='(' + year + ')')
         item.set_xbmc_list_item_obj(xbmcListItem)
         response_obj.addListItem(item)
+        
+    if current_page > 0 and current_page < total_pages:
+        next_page = current_page + 1
+        item = ListItem()
+        item.add_request_data('page', next_page)
+        item.add_request_data('infoUrl', request_obj.get_data()['infoUrl'])
+        item.set_next_action_name('Next_Page')
+        xbmcListItem = xbmcgui.ListItem(label='  ---- next page ----  #' + str(next_page) + ' ->')
+        item.set_xbmc_list_item_obj(xbmcListItem)
+        response_obj.addListItem(item)
+        
     response_obj.set_xbmc_content_type('movies')
 
-        
+'''
+Cached function to retrieve category specific movies
+'''
+def retrieveCategoryMovies(infoUrl):
+    titles = []
+    contentDiv = BeautifulSoup.SoupStrainer('div', {'dir':'ltr'})
+    soup = HttpUtils.HttpClient().getBeautifulSoup(url=infoUrl, parseOnlyThese=contentDiv)
+    for aTag in soup.findAll('a', {'href':re.compile('http://www.sominaltv(theater|films).com(.+?).html')}):
+        if not re.search('^(?!\()', aTag.getText()):
+            continue
+        titleInfo = aTag.getText()
+        movieInfo = re.compile("(.+?)\((\d+)\)").findall(titleInfo)
+        if not len(movieInfo) > 0:
+            continue
+        moviePageUrl = aTag['href']
+        title = {}
+        title['info'] = titleInfo
+        title['link'] = moviePageUrl
+        titles.append(title)
+    return titles
+
 def retieveMovieStreams(request_obj, response_obj):
     soup = None
     if request_obj.get_data().has_key('movieInfoUrl'):
