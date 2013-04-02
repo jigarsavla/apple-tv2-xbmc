064e1b2ae68921bc41ad3a33992d96574aa4dd7e
ajdeveloped@gmail.com <ajdeveloped@gmail.com@53b91c4f-db84-3a66-376e-a7ef90917ce7>
2013-04-02 04:50:29 +0000

    Added YouTube channels to Filmi By Nature

--- addons/plugin.video.filmibynature/mymoves/Home.py
+++ addons/plugin.video.filmibynature/mymoves/Home.py
@@ -14,7 +14,16 @@ def displayMenuItems(request_obj, response_obj):
     movie_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='Movies_V1.png')
     item = ListItem()
     item.set_next_action_name('Movie')
-    xbmcListItem = xbmcgui.ListItem(label='Movie', iconImage=movie_icon_filepath, thumbnailImage=movie_icon_filepath)
+    xbmcListItem = xbmcgui.ListItem(label='Movies', iconImage=movie_icon_filepath, thumbnailImage=movie_icon_filepath)
+    item.set_xbmc_list_item_obj(xbmcListItem)
+    response_obj.addListItem(item)
+    
+    
+    # A-Z
+    az_movie_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='AZ_Dir_V1.png')
+    item = ListItem()
+    item.set_next_action_name('AZ')
+    xbmcListItem = xbmcgui.ListItem(label='Movies A to Z', iconImage=az_movie_icon_filepath, thumbnailImage=az_movie_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
     response_obj.addListItem(item)
     
@@ -23,24 +32,17 @@ def displayMenuItems(request_obj, response_obj):
     item = ListItem()
     item.set_next_action_name('Trailer')
     item.add_request_data('categoryUrlSuffix', 'Trailers')
-    xbmcListItem = xbmcgui.ListItem(label='TRAILERS', iconImage=trailer_movie_icon_filepath, thumbnailImage=trailer_movie_icon_filepath)
+    xbmcListItem = xbmcgui.ListItem(label='Trailers', iconImage=trailer_movie_icon_filepath, thumbnailImage=trailer_movie_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
     response_obj.addListItem(item)
     
-    # A-Z
-    az_movie_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='AZ_Dir_V1.png')
-    item = ListItem()
-    item.set_next_action_name('AZ')
-    xbmcListItem = xbmcgui.ListItem(label='A to Z', iconImage=az_movie_icon_filepath, thumbnailImage=az_movie_icon_filepath)
-    item.set_xbmc_list_item_obj(xbmcListItem)
-    response_obj.addListItem(item)
     
-    # LIVE TV item
+    # YouTube TV item
     youtube_icon_filepath = AddonUtils.getCompleteFilePath(baseDirPath=AddonContext().addonPath, extraDirPath=AddonUtils.ADDON_ART_FOLDER, filename='YouTube_V1.png')
     item = ListItem()
     item.set_next_action_name('YouTube')
-    xbmcListItem = xbmcgui.ListItem(label='YouTube', iconImage=youtube_icon_filepath, thumbnailImage=youtube_icon_filepath)
+    xbmcListItem = xbmcgui.ListItem(label='YouTube Channels', iconImage=youtube_icon_filepath, thumbnailImage=youtube_icon_filepath)
     item.set_xbmc_list_item_obj(xbmcListItem)
-#     response_obj.addListItem(item)
+    response_obj.addListItem(item)
 
 
