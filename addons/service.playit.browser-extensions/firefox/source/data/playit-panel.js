16c5cc6ad8eacf5fb6b00bad717b1c98dcedacfc
ajdeveloped@gmail.com <ajdeveloped@gmail.com@53b91c4f-db84-3a66-376e-a7ef90917ce7>
2013-04-17 02:32:45 +0000

    New PlayIt add-on version.

--- addons/service.playit.browser-extensions/firefox/source/data/playit-panel.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("playit").onclick=function(){
-    data = {
-        'serviceAddress':document.getElementById('serviceAddress').value,
-        'servicePort':document.getElementById('servicePort').value
-    }
-    console.log(data.serviceAddress)
-    self.port.emit("playItEvent", data);
-}
