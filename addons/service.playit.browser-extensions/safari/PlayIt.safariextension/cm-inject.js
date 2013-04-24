14a1e7cbb0d3bee52b1bb4e83bdfd481631f901a
ajdeveloped@gmail.com <ajdeveloped@gmail.com@53b91c4f-db84-3a66-376e-a7ef90917ce7>
2013-04-24 03:55:32 +0000

    Corrected display of context menu item for PlayIt when Anchor is parentNode.

--- addons/service.playit.browser-extensions/safari/PlayIt.safariextension/cm-inject.js
+++ addons/service.playit.browser-extensions/safari/PlayIt.safariextension/cm-inject.js
@@ -1,10 +1,19 @@
 //Context menu listener
 document.addEventListener("contextmenu", handleContextMenu, false);
-//This function adds information about the hyperlink on which context menu opened.
+// This function adds information about the hyperlink on which context menu
+// opened.
 function handleContextMenu(event) {
-	if(event.target.nodeName == 'A'){
-		safari.self.tab.setContextMenuEventUserInfo(event, [event.target.href,event.target.textContent.trim()]);
-	}else if(event.target.parentNode.nodeName == 'A'){
-		safari.self.tab.setContextMenuEventUserInfo(event, [event.target.parentNode.href,event.target.parentNode.textContent.trim()]);
+	var linkElement = undefined;
+	var currentElement = event.target;
+	while (currentElement != null) {
+		if (currentElement.nodeName == 'A') {
+			linkElement = currentElement;
+			break;
+		}
+		currentElement = currentElement.parentNode;
+	}
+	if (linkElement != undefined) {
+		safari.self.tab.setContextMenuEventUserInfo(event, [ linkElement.href,
+				linkElement.textContent.trim() ]);
 	}
 }
