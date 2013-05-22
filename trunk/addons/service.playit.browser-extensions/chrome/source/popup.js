7f9158f9d648f240c4a13c4a3a296db459f83f47
ajdeveloped@gmail.com <ajdeveloped@gmail.com@53b91c4f-db84-3a66-376e-a7ef90917ce7>
2013-05-22 07:43:56 +0000

    PlayIt Frame View alignment corrected.

--- trunk/addons/service.playit.browser-extensions/chrome/source/popup.js
+++ trunk/addons/service.playit.browser-extensions/chrome/source/popup.js
@@ -9,7 +9,27 @@ function playItFrameView() {
 	playIt.playItFrameViewEnabler();
 }
 
-clicker = document.querySelector('#playIt').addEventListener('click',
-		playActiveURL, false);
-dialog = document.querySelector('#frameView').addEventListener('click',
-		playItFrameView, false);
+function trackButtonClick(e) {
+	_gaq.push([ '_trackEvent', e.target.id, 'clicked' ]);
+};
+
+document.querySelector('#playIt').addEventListener('click', playActiveURL);
+document.querySelector('#frameView').addEventListener('click', playItFrameView);
+
+var buttons = document.querySelectorAll('button');
+for ( var i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', trackButtonClick);
+}
+
+var _gaq = _gaq || [];
+_gaq.push([ '_setAccount', 'UA-22779680-5' ]);
+_gaq.push([ '_trackPageview' ]);
+
+(function() {
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+})();
