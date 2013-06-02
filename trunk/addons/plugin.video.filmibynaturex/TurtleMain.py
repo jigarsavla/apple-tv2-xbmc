14dc84b54dd38770d505ef00d062d3b64234da0d
ajdeveloped@gmail.com <ajdeveloped@gmail.com@53b91c4f-db84-3a66-376e-a7ef90917ce7>
2013-06-02 04:34:33 +0000

    Error handling in video add-ons.

--- trunk/addons/plugin.video.filmibynaturex/TurtleMain.py
+++ trunk/addons/plugin.video.filmibynaturex/TurtleMain.py
@@ -4,7 +4,12 @@ Created on Nov 20, 2012
 @author: ajju
 '''
 
-import TurtlePlugin
 
-if __name__ == '__main__':
-    TurtlePlugin.start('plugin.video.filmibynaturex', '2.3.2')
+try:
+    import TurtlePlugin
+except:
+    import xbmcgui  # @UnresolvedImport
+    dialog = xbmcgui.Dialog()
+    dialog.ok('[B][COLOR red]ALERT: [/COLOR][/B] RESTART XBMC', 'A new update has recently installed or add-on reconfigured.', 'Please restart XBMC to reflect the changes.', 'You will not be able to access until restart.')
+
+TurtlePlugin.start('plugin.video.filmibynaturex', '2.3.2')
