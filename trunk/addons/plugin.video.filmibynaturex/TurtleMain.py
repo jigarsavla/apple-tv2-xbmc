f65fe03a9280d2895cc629c0d078bfdc70144feb
ajdeveloped@gmail.com <ajdeveloped@gmail.com@53b91c4f-db84-3a66-376e-a7ef90917ce7>
2013-06-08 22:53:08 +0000

    Merged from ajdeveloped branch.

--- trunk/addons/plugin.video.filmibynaturex/TurtleMain.py
+++ trunk/addons/plugin.video.filmibynaturex/TurtleMain.py
@@ -12,4 +12,4 @@ except:
     dialog = xbmcgui.Dialog()
     dialog.ok('[B][COLOR red]ALERT: [/COLOR][/B] RESTART XBMC', 'A new update has recently installed or add-on reconfigured.', 'Please restart XBMC to reflect the changes.', 'You will not be able to access until restart.')
 
-TurtlePlugin.start('plugin.video.filmibynaturex', '2.3.2')
+TurtlePlugin.start('plugin.video.filmibynaturex', '2.3.7')
